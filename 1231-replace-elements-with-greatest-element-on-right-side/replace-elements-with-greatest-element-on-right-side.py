a5f98d76f41d32d71c72acf937ecde15a3e6cd0f
Asela Dasanayaka <rukmal.tb@gmail.com>
2025-01-11 16:32:56 +0530

    Time: 50 ms (19.79%) | Memory: 19 MB (12.55%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -3,14 +3,13 @@ from collections import deque
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
 
-        nums = deque([])
+        nums = deque([-1])
         right_max = arr[-1]
         
         for i in range(len(arr)-2,-1,-1):           
             nums.appendleft(right_max)
             right_max = max(right_max, arr[i])
 
-        nums.append(-1)
         return list(nums)
 
         
