e217836dee696a3c5b7eb9b75acc899e6e47ea74
Asela Dasanayaka <rukmal.tb@gmail.com>
2025-05-28 21:53:26 +0530

    Time: 9 ms (76.86%) | Memory: 17.8 MB (63.63%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,12 +1,5 @@
+from collections import Counter
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_counter, t_counter = {}, {}
-
-        for c in s:
-            s_counter[c] = s_counter.get(c, 0) + 1
-
-        for c in t:
-            t_counter[c] = t_counter.get(c, 0) + 1
-
-        return s_counter == t_counter
-        
+        return Counter(s) == Counter(t)
