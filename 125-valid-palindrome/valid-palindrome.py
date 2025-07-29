a03e4b3a22e049b14ef76e451a68f9d5c94074a4
Asela Dasanayaka <rukmal.tb@gmail.com>
2025-07-29 22:40:45 +0100

    Time: 3 ms (99.01%) | Memory: 18.4 MB (38.57%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        new_s = ''.join(ch for ch in s if ch.isalnum()).lower()
 
-        process = [ch.lower() for ch in s if ch.isalnum()]
-
-        return process == process[::-1]
+        return new_s == new_s[::-1]
+        
