108fc1dcbec9228a609c4d6021a69737c2910691
Asela Dasanayaka <rukmal.tb@gmail.com>
2025-07-31 10:57:50 +0100

    Time: 2580 ms (7.21%) | Memory: 20 MB (89.88%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -6,38 +6,28 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]: 
 
-        if not lists or len(lists) == 0:
+        if not lists:
             return None
 
-        while len(lists) >1:
-            combine_lists = []
+        if len(lists) == 1:
+            return lists[0]
 
-            for i in range(0, len(lists), 2):
-                l1 = lists[i]
-                l2 = lists[i + 1] if (i + 1) < len(lists) else None
-                combine_lists.append(self.mergeList(l1, l2))
+        dummy_head = ListNode()
+        curr = dummy_head
 
-            lists = combine_lists
+        state = [l.val if l else None for l in lists]
 
-        return lists[0]
-        
+        while not all(s is None for s in state):
 
-    def mergeList(self, l1, l2):
+            min_value = min(x for x in state if x is not None )  
+            min_index = state.index(min_value)
 
-        dummy = curr = ListNode()
+            curr.next = ListNode(val=min_value)
 
-        while l1 and l2:
-            if l1.val < l2.val:
-                curr.next = l1
-                l1 = l1.next
-            else:
-                curr.next = l2
-                l2 = l2.next
+            next_list_node = lists[min_index].next if lists[min_index] else None
+            state[min_index] = next_list_node.val if next_list_node else None
+
+            lists[min_index] = next_list_node if next_list_node else None
             curr = curr.next
 
-        if l1:
-            curr.next = l1
-        if l2:
-            curr.next = l2
-
-        return dummy.next
+        return dummy_head.next
