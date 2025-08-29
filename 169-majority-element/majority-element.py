b156d19c1725023441b2d14816584b49cd36c873
Asela Dasanayaka <rukmal.tb@gmail.com>
2025-08-29 11:51:53 +0100

    Time: 15 ms (14.19%) | Memory: 19.3 MB (50.60%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        major = len(nums)/2
         counter = {}
+
         for n in nums:
             counter[n] = counter.get(n,0) + 1
+            if counter[n]> major:
+                return n
         
-        for item in counter.items():
-            if item[1] > len(nums)/2 :
-                return item[0]
 
         
